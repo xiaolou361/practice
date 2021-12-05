81235196df0dd82fe30da624f39fc88fa0a84fc0
xiao <95233133+xiaolou361@users.noreply.github.com>
2021-12-05 23:44:53 +0800

    x的n次方

--- main.c
+++ main.c
@@ -1,25 +1,20 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <string.h>
-int str(char a[20], char b[20]) {
-    int i, j, k;
-    j = strlen(a);
-    k = j - 1;
-    for (i = 0; i < j; i++) {
-        b[i] = a[k];
-        k--;
+#include <math.h>
+double sqr(int x, int n) {
+    int i,s=1;
+    for (i = n; i >0; i--) {
+        if (n != 1)
+            s *= x;
+        else
+            return 1;
     }
+    return s;
 }
 int main()
 {
-    char a[20],b[20];
-    int i, l;
-    scanf("%s", a);
-    l = strlen(a);
-    str(a, b);
-    for (i = 0; i < l; i++) {
-        printf("%c", b[i]);
-    }
- 
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%.2f\n",sqr(b,a));
     return 0;
 }
