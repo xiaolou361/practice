eb4e286624df942044cc64217fdf8765f0eaba49
xiao <95233133+xiaolou361@users.noreply.github.com>
2021-12-10 00:42:12 +0800

    Add files via upload

--- main.c
+++ main.c
@@ -1,23 +1,33 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-//int main() {
-//	int i, s = 1,n, sum = 0;
-//	for (n = 1; n <= 3; n++) {
-//
-//		for (i = 1; i <= n; i++) {
-//			s *= i;
-//		}
-//			sum += s;
-//			s = 1;
+//int main()
+//{
+//	int i = 0;
+//	for (i = 1; i <= 10; i++) {
+//		if (i == 5)
+//			continue;
+//		printf("%d ", i);
 //	}
-//	printf("%d", sum);
+//}
+//int main() {
+//	int i = 0, j = 0;
+//	for (;i<10;i++) {
+//		for (; j < 10; j++) {  //当i=0，j=10时，j=10并未销毁
+//			printf("hehehe\n");
+//		}
+//	
+//	}
+//}
+//int main() {
+//	int x, y;
+//	for (x = 0, y = 0; x < 2 && y < 5; ++x, ++y) {
+//		printf("hehe\n");
+//	}
 //}
 int main() {
-	int  s = 1, n, sum = 0;
-	for (n = 1; n <= 3; n++) {
-        s *= n;
-		sum += s;
+	int i;
+	for (i = 0; i = 0/*将0赋值给i，i为假*/; i++) {
+		printf("haha\n");
 	}
-	printf("%d", sum);
 }
